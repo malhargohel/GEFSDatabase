c2ec4ef785f3a97fd56789057e3d1aa1bb25f1fa
ac98824 <ac98824@8CG8163KWC>
2021-11-26 11:19:55 +1300

    Add project files.

---
+++ GEFSDatabase1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GEFSDatabase1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
